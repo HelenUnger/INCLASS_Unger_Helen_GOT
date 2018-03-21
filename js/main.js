91ee4848abbd30a130a82fb30bb1133cd008638a
Unger <h_unger@tss-hr420-dm25.local>
2018-03-21 15:45:08 -0400

    custom player

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 (() => {
   console.log('js file loded!');
-});
+
+  const vidPlayer = document.querySelector('video'),
+        playButton = document.querySelectorAll('button')[0],
+        pauseButton = document.querySelectorAll('button')[1],
+        rewindButton = document.querySelectorAll('button')[2];
+
+function volOn() {
+  vidPlayer.muted= false;
+}
+
+function volOff() {
+vidPlayer.muted= true;
+}
+
+function rewindVid() {
+  vidPlayer.currentTime = 0;
+}
+
+function playVid() {
+  vidPlayer.play();
+}
+
+function pauseVid() {
+  vidPlayer.pause();
+}
+
+vidPlayer.addEventListener('mouseover', volOn);
+vidPlayer.addEventListener('mouseout', volOff);
+
+playButton.addEventListener('click', playVid);
+pauseButton.addEventListener('click', pauseVid);
+rewindButton.addEventListener('click', rewindVid);
+
+})();
